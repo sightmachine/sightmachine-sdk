07ea90c481b88ded9d34df92bde6ce74896312ea
Kurt DeMaagd <kurt@sightmachine.com>
2021-01-20 09:32:27 -0500

    Fix requirement version numbers

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 install_requires = [
-    "numpy==1.18.4",
-    "requests==2.24.0",
-    "pytest==6.0.2",
+    "numpy>=1.18.4",
+    "requests>=2.24.0",
+    "pytest>=6.0.2",
     "pandas>=0.25.2",
-    "mock==4.0.2",
-    "importlib-resources==3.0.0"
+    "mock>=4.0.2",
+    "importlib-resources>=3.0.0"
 ]
 
 setup(
     name="smsdk",
-    version="1.0",
+    version="1.0.1",
     packages=find_packages(exclude=["test*"]),
     include_package_data=True,
     install_requires=install_requires,
@@ -23,6 +23,6 @@ setup(
     author="Sight Machine",
     author_email="support@sightmachine.com",
     url="http://sightmachine.com/",
-    description="AI Data Pipeline tool",
+    description="Sight Machine SDK",
     python_requires=">=3.6.8",
 )

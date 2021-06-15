77cce47953e8cddc86807c3352178f8f2971b4ce
Kurt DeMaagd <demaagdk@gmail.com>
2021-06-15 13:36:14 -0400

    Increment version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ install_requires = [
 
 setup(
     name="smsdk",
-    version="1.0.8",
+    version="1.0.9",
     packages=find_packages(exclude=["test*"]),
     include_package_data=True,
     install_requires=install_requires,

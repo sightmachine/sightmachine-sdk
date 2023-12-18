79d8e5ae0fc056d8adc9308a26d50af024aac9cc
Murali Krishna <45696741+JMkrish@users.noreply.github.com>
2023-12-18 19:38:03 +0530

    ENG-3777: Run Python notebooks in SDK Circle-CI (#62)
    * ENG-3777 : Making Python notebooks as part of unit test
    
    * Moving 'ipywidgets' & 'plotly' libraries from requirements-codecoverage.txt to requirements.txt.
    Updated tenant's API-KEY and SECRETE.

--- smsdk/const.py
+++ smsdk/const.py
@@ -1,3 +1,3 @@
-API_KEY = "cea8969e-2acd-4ff4-b88b-e6b62d5cd857"
-API_SECRETE = "sma_nBy04LSDJhwxXNct8R8pREkwzbUTWjU4ZmQj7Y2ApAi_"
+API_KEY = "5a73aa5a-1962-4df9-b56e-4a59462f0f00"
+API_SECRETE = "sma_FajgH3VbPu68gwy0PzccvhyGRyy1a8CCHhhvy6ooeg1O_"
 TENANT = "demo-sdk-test"

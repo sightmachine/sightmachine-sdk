18e6978a7d2f571b267ba08cb1bec69cf8946643
Yashadatt Sawant <yashadatt.sawant@velotio.com>
2024-04-25 13:58:28 +0530

    GMI-353: New api format

--- smsdk/smsdk_entities/dashboard/dashboard_v1.py
+++ smsdk/smsdk_entities/dashboard/dashboard_v1.py
@@ -46,8 +46,9 @@ class DashboardData(SmsdkEntities, MaSession):
         )
         print(f"=========={url}==============")
 
-        records = self._get_records_v1(url, method="get", results_under="objects")
         records_new = self._get_records(url, method="get")
+        # records = self._get_records_v1(url, method="get", results_under="objects")
+
         log.warning(f"=========={records_new}==============")
 
         if records_new:
